612329dafd87af49264fef1ad20e26904a8f5121
MichaelKo <viacheslav.sychov@gmail.com>
2022-11-24 12:23:24 +0100

    Update lens-clean.rb

--- Casks/lens-clean.rb
+++ Casks/lens-clean.rb
@@ -1,7 +1,7 @@
 cask "lens-clean" do
     arch = Hardware::CPU.intel? ? "" : "-arm64"
   
-    version "2022.11.101953"
+    version "2022.11.211937"
   
     url "https://api.k8slens.dev/binaries/Lens-#{version}-latest#{arch}.dmg"
     name "LensClean"
